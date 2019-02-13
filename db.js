ba92e8c925b5708821f40abc6e0103c94b85f5f3
BettinaThies <bettina.thies@outlook.de>
2019-02-13 17:56:49 +0100

    server is runing now

--- db.js
+++ db.js
@@ -7,16 +7,17 @@ module.exports.getAllCities = function getAllCities(){
     return db.query('SELECT * FROM cities');
 };
 
-module.exports.addCity = function addCity(city){
+module.exports.addCity = function addCity(city, state, country) {
     db.query(
-        'INSERT INTO cities (city) VALUES ($1)'),
-    [ city, state ]
-    )
-}
+        'INSERT INTO cities (city, state, country) VALUES ($1, $2, $3)',
+        [ city, state, country ]
+    );
+};
 
+/*
 app.post('/create-new-city', (req, res) =>{
     // again this
     db.addCity('Unna', 'NRW').then(() =>{
 
-    })
-})
+    });
+});*/
--- index.js
+++ index.js
@@ -3,9 +3,29 @@ const app = express();
 
 const db = require ('./db');
 
+/// HANDLEBARS DO NOT TOUCH CODE BELOW /////
 var hb = require('express-handlebars');
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
+/// HANDLEBARS DO NOT TOUCH CODE ABOVE /////
+
+app.use(
+    express.static('./wintergreen-petition')
+);
+
+// LINKINK TO THE CSS
+app.use(
+    express.static('./public')
+);
+
+
+// GET PETITION//
+app.get("/petition", function(req, res) {
+    res.render("petition", {
+        layout: "main"
+    });
+});
+
 
 app.get('/get-cities', (req, res) =>{
     //this is just for demo purposes
@@ -19,4 +39,4 @@ app.get('/get-cities', (req, res) =>{
     });
 });
 
-app.listen(8080, () => console.log()('Petition listening!'));
+app.listen(8080, () => console.log('Petition listening!'));
